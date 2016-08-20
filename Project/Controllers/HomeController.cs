bc52cd4c39d9c82de72dfd177b929656b3e97c6e
SimSimnv <simsimnv@abv.bg>
2016-08-20 17:38:08 +0300

    Initial Commit
    -Implemented user registration, login and logout, along with
    controller,view and crud operations for the Post class.
    -Enabled automatic migrations to database.
    -Added relationship between users and posts

---
+++ Project/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebProject.Models;
+using System.Data.Entity;
+
+namespace WebProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            var posts = db.Posts.Include(p => p.Author).OrderByDescending(p => p.Date).Take(4).ToList();
+
+            return View(posts);
+        }
+    }
+}
---
+++ Project/Models/Post.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using WebProject.Models;
+using Microsoft.AspNet.Identity;
+
+namespace WebProject.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public ApplicationUser Author { get; set; }
+
+
+    }
+}
