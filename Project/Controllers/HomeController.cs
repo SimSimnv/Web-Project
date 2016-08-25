f357087c75a1ae4903e5e2bd28fd6d518377f6f7
SimSimnv <simsimnv@abv.bg>
2016-08-25 22:08:48 +0300

    Design update
    -Changed the default bootstrap
    -Updated css
    -Added latest 5 comments sidebar in the homepage

--- Project/Controllers/HomeController.cs
+++ Project/Controllers/HomeController.cs
@@ -14,6 +14,9 @@ namespace WebProject.Controllers
         public ActionResult Index()
         {
             var posts = db.Posts.Include(p => p.Author).OrderByDescending(p => p.Date).Take(4).ToList();
+            var comments = db.Comments.Include(c => c.Author).Include(c => c.Post).OrderByDescending(c => c.Date).Take(5);
+            ViewBag.Comments = comments;
+            
             return View(posts);
         }
     }
